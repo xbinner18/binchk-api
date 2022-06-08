487ce1316881d866e6f91cae659196e76e6e0dec
Nitin1818 <74262832+xbinner18@users.noreply.github.com>
2022-06-09 01:45:34 +0530

    Cleanup

--- bindb.py
+++ bindb.py
@@ -1,32 +1,31 @@
 import httpx
-import re
 from bs4 import BeautifulSoup as bs
 
 
 async def bin_scrape(binovt):
     url = 'https://binov.net/'
-    x = re.sub(r'[^0-9]', '', binovt)
-    if len(x) < 6:
+    if len(binovt) < 6:
         return {"status": False, "error": "INVALID DATA PROVIDED 6 DIGITS REQUIRED"}
     try:
         async with httpx.AsyncClient() as client:
             r = await client.post(url,
                                   data={
-                                      'BIN': x[:6],
+                                      'BIN': binovt[:6],
                                       'BANK': 1,
                                       'COUNTRY': 1,
                                   })
             soup = bs(r, features='lxml')
             k = soup.find('table', width="900px")
+            al = k.findAll('td')
 
             data = {}
             data["status"] = True
-            data["bin"] = x[:6]
-            data["brand"] = k.findAll('td')[7].get_text()
-            data["bank"] = k.findAll('td')[8].get_text()
-            data["type"] = k.findAll('td')[9].get_text()
-            data["level"] = k.findAll('td')[10].get_text()
-            data["country"] = k.findAll('td')[11].get_text()
+            data["bin"] = binovt[:6]
+            data["brand"] = al[7].get_text()
+            data["bank"] = al[8].get_text()
+            data["type"] = al[9].get_text()
+            data["level"] = al[10].get_text()
+            data["country"] = al[11].get_text()
             return data
     except Exception as e:
         return {"status": False, "error": e}
